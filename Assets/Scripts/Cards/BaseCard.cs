55f8ecb5fdce4d8060915d6e928c4dc153659af5
Edwin Lin <elin4527@gmail.com>
2020-06-14 17:40:41 -0400

    Add Mirror Plugin
    Add Mirror Plugin for network multiplayer

---
+++ Assets/Scripts/Cards/BaseCard.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCard : MonoBehaviour
+{
+    public string Name { get; set; }
+
+    private int manaCost;
+    public int ManaCost
+    {
+        get { return manaCost; }
+        set { if (value >= 0) { manaCost = value; } }
+    }
+
+    [SerializeField]
+    private List<CardEffect> cardEffects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void AddCardEffect(CardEffect effect)
+    {
+        cardEffects.Add(effect);
+    }
+}
